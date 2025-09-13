8e279d8b58aec25620594c993f8ad63b6912bcbe
Vaishali-Anapayan <vanapaya@uwaterloo.ca>
2025-09-12 21:57:06 -0400

    Fixing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'vaishali-anapayan',
+  base: 'vaishali-anapayan.github.io',
   plugins: [react()],
 })
